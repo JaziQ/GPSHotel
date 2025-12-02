bc854d2dad98363da318967509993d7c05a83b76
Jazi <gleb_andreev01@mail.ru>
2025-12-02 20:03:18 +0300

    Init

---
+++ src/main/java/jazi/gpshotel/GpsHotelApplication.java
@@ -0,0 +1,13 @@
+package jazi.gpshotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jazi/gpshotel/GpsHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package jazi.gpshotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
