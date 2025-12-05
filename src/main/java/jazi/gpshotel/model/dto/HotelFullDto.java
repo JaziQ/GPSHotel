1f8cdb71dbcf21227447e3109910e631ec71b643
Jazi <gleb_andreev01@mail.ru>
2025-12-05 19:52:26 +0300

    Add dto swagger documentation

--- src/main/java/jazi/gpshotel/model/dto/HotelFullDto.java
+++ src/main/java/jazi/gpshotel/model/dto/HotelFullDto.java
@@ -1,5 +1,6 @@
 package jazi.gpshotel.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -12,31 +13,42 @@ import lombok.Data;
 import java.util.List;
 
 @Data
+@Schema(description = "Full hotel information")
 public class HotelFullDto {
 
+    @Schema(description = "Unique hotel identifier", example = "123")
     @NotNull
     private Long id;
 
+    @Schema(description = "Hotel name", example = "DoubleTree by Hilton Minsk", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotBlank(message = "Name is mandatory")
     private String name;
 
+    @Schema(description = "Hotel description", example = "The DoubleTree by Hilton Hotel Minsk " +
+            "offers 193 luxurious rooms in the Belorussian capital...")
     @Size(max = 2000, message = "Description cannot exceed 2000 characters")
     private String description;
 
+    @Schema(description = "Hotel brand", example = "Hilton")
     @Size(max = 100, message = "Brand name cannot exceed 100 characters")
     private String brand;
 
+    @Schema(description = "Hotel address")
     @NotNull(message = "Address is mandatory")
     @Valid
     private Address address;
 
+    @Schema(description = "Contact information")
     @NotNull(message = "Contacts are mandatory")
     @Valid
     private Contacts contacts;
 
+    @Schema(description = "Check-in, check-out time", example = "14:00")
     @Valid
     private ArrivalTime arrivalTime;
 
+    @Schema(description = "List of amenities", example = "[\"Free parking\", \"Non-smoking rooms\"",
+            requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private List<String> amenities;
 
 }
--- src/main/java/jazi/gpshotel/model/dto/HotelShortDto.java
+++ src/main/java/jazi/gpshotel/model/dto/HotelShortDto.java
@@ -1,25 +1,33 @@
 package jazi.gpshotel.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+@Schema(description = "Short hotel information")
 @Data
 public class HotelShortDto {
 
+    @Schema(description = "Unique hotel identifier", example = "123")
     @NotNull
     private Long id;
 
+    @Schema(description = "Hotel name", example = "Grand Hotel Moscow", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotBlank(message = "Name is mandatory")
     private String name;
 
+    @Schema(description = "Hotel description", example = "The DoubleTree by Hilton Hotel Minsk " +
+            "offers 193 luxurious rooms in the Belorussian capital...")
     @Size(max = 2000, message = "Description cannot exceed 2000 characters")
     private String description;
 
+    @Schema(description = "Full address", example = "9 Pobediteley Avenue, Minsk, 220004, Belarus")
     @NotNull(message = "Address is mandatory")
     private String address;
 
+    @Schema(description = "Phone number", example = "+375 17 309-80-00")
     @NotNull(message = "Phone number is mandatory")
     private String phone;
 
